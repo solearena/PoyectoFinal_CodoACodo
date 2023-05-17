0de64518dc8a9436a5cbfee5ce708b123810c2ff
solearena <solearena@gmail.com>
2023-05-17 13:28:38 -0300

    Se cambia termina el index y se agrega el enmaquetado en el portfolio

--- assets/js/bienvenida.js
+++ assets/js/bienvenida.js
@@ -1,4 +1,5 @@
 function bienvenida(){
     var name = document.getElementById("nombre").value;
     document.getElementById("demo").innerHTML = "Bienvenida/o " + name + "!";
+    document.getElementById("nombre").value = ""
 }
